d219660d890192dbe7cc7b26ae212c746a5e789c
Jose Ricardo <josericardo29@hotmail.es>
2018-11-29 19:03:47 -0400

    Este ultimo nos sirve particularmente para la entidad que almacenara los datos sobre los autos agregados al sistema

---
+++ Models/Car.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace IdentityScaffoldingII.Models 
+{
+    public class Car 
+    {
+        public int ID { get; set; }
+        public string Mark { get; set; }
+        public string Model { get; set; }
+        public DateTimeOffset DayMade { get; set; }
+        public string Matricula { get; set; }
+     //ESTO ES UNA PRUEBITA.
+    
+    }
+}
